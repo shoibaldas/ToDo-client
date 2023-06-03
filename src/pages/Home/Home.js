47ba1e365ee25c8bb7513e763241c164bd191063
Shoibal Das <your_email@example.com>
2023-06-03 13:15:37 +0600

    project structure for todo-client app is initialized

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Sidebar from '../../components/Sidebar/Sidebar';
+
+const Home = () => {
+    return (
+        <div>
+            <Sidebar></Sidebar>
+        </div>
+    );
+};
+
+export default Home;
